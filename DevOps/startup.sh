64d99382d25a747dd0b916d8288d075a149c7c89
Krzysztof Grzegrzulka <19797451+krzysztof-grzegrzulka@users.noreply.github.com>
2022-06-16 22:15:18 +0200

    changes realted to properly start container

--- DevOps/startup.sh
+++ DevOps/startup.sh
@@ -3,8 +3,7 @@
 # exec "git clone https://github.com/ADZD-2021/AZnS.git"
 # exec "git checkout devops_dev"
 # cd target/
-pwd
-cd /AZnS/target/
+cd /AZnS/DevOps/
 BINARY_NAME=$(ls | grep war)
 exec gosu root /bin/bash -c "
 java -jar {{ BINARY_NAME }}.war
